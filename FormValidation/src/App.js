1dfb2aa7c12808ea7c84b7a2d889ccc8cdf6aa27
NAVEEN KUMAR <nk946432@gmail.com>
2024-11-19 12:19:35 +0530

    creating react dom

--- FormValidation/src/App.js
+++ FormValidation/src/App.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import Form from './Component/Form'
+import Home from './Home'
+import Header from './Header'
+import Nav from './Nav'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Footer from './Footer'
+import Missing from './Missing'
+import { Route, Routes } from 'react-router'
+import { Link } from 'react-router-dom'
 
 
 const App = () => {
   return (
     <div>
-      <Form/>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/newpost'>New Post</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/newpost' element={<NewPost/>}/>
+      </Routes>
+   {/*   <Header/>
+     <Nav/>
+     <Home/>
+     <NewPost/>
+     <PostPage/>
+     <About/>
+     <Missing/>
+     <Footer/> */}
     </div>
   )
 }
